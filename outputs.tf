e7b45548cdb9335a0786fdd77aad96f7e5fec970
Rohith Jayawardene <gambol99@gmail.com>
2024-04-01 20:55:00 +0100

    chore: adding additional outputs

--- outputs.tf
+++ outputs.tf
@@ -4,6 +4,16 @@ output "connectivity_type" {
   value       = var.connectivity_config
 }
 
+output "egress_vpc_id" {
+  description = "The ID of the VPC that is used for egress traffic."
+  value       = local.enable_egress_creation ? module.egress_vpc.vpc_id : null
+}
+
+output "ingress_vpc_id" {
+  description = "The ID of the VPC that is used for ingress traffic."
+  value       = local.enable_ingress_creation ? module.ingress_vpc.vpc_id : null
+}
+
 output "transit_gateway_id" {
   description = "The ID of the transit gateway."
   value       = module.tgw.ec2_transit_gateway_id
