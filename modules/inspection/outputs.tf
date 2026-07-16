ed8dc8893e5a799829a55c81f17ca83927c07454
Rohith Jayawardene <gambol99@gmail.com>
2026-07-16 19:17:17 +0100

    feat(outputs): added the hosted zones to the output

--- modules/inspection/outputs.tf
+++ modules/inspection/outputs.tf
@@ -93,6 +93,11 @@ output "ingress_vpc_id_rt_attributes_by_type_by_az" {
   value       = local.enable_ingress ? module.ingress_vpc[0].rt_attributes_by_type_by_az : null
 }
 
+output "endpoints_hosted_zones" {
+  description = "The hosted zones of the endpoints VPC."
+  value       = local.enable_endpoints ? module.endpoints[0].hosted_zone_map : null
+}
+
 output "endpoints_vpc_id" {
   description = "The ID of the VPC that is used for endpoint traffic."
   value       = local.enable_endpoints ? module.endpoints[0].vpc_id : null
@@ -107,4 +112,3 @@ output "endpoints_vpc_id_rt_attributes_by_type_by_az" {
   description = "The route table attributes of the endpoints VPC."
   value       = local.enable_endpoints ? module.endpoints[0].rt_attributes_by_type_by_az : null
 }
-
