f87be70bc07f19452db0586a9167c648f45468a0
Rohith Jayawardene <gambol99@gmail.com>
2026-07-17 18:28:27 +0100

    chore(dep): updated the network module version to 0.7.0

--- modules/inspection/endpoints.tf
+++ modules/inspection/endpoints.tf
@@ -3,7 +3,7 @@
 module "endpoints_vpc" {
   count   = local.enable_endpoints ? 1 : 0
   source  = "appvia/network/aws"
-  version = "0.6.14"
+  version = "0.7.0"
 
   availability_zones                     = var.services.endpoints.network.availability_zones
   enable_default_route_table_association = false
